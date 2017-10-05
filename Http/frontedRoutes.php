42169de7d4c45c1198972a0a423d9c0e45fb5c86
aniliso <sagsoz06@gmail.com>
2017-10-05 21:19:39 +0300

    Disable Portfolio frontedRoutes

--- Http/frontedRoutes.php
+++ Http/frontedRoutes.php
@@ -3,13 +3,13 @@
 use Illuminate\Routing\Router;
 
 /** @var Router $router */
-$router->group(['prefix' =>''], function (Router $router) {
-    $router->get(LaravelLocalization::transRoute('portfolio::routes.brand.index'), [
-        'as'         => 'brand.index',
-        'uses'       => 'PublicController@index'
-    ]);
-    $router->get(LaravelLocalization::transRoute('portfolio::routes.brand.slug'), [
-        'as'         => 'brand.slug',
-        'uses'       => 'BrandController@brand'
-    ]);
-});
+//$router->group(['prefix' =>''], function (Router $router) {
+//    $router->get(LaravelLocalization::transRoute('portfolio::routes.brand.index'), [
+//        'as'         => 'brand.index',
+//        'uses'       => 'PublicController@index'
+//    ]);
+//    $router->get(LaravelLocalization::transRoute('portfolio::routes.brand.slug'), [
+//        'as'         => 'brand.slug',
+//        'uses'       => 'BrandController@brand'
+//    ]);
+//});
